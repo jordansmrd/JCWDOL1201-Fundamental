bdab68206441de25ba4f0bd7491595dc1de15d7a
Jordan Sumardi <jordansumardi@hotmail.com>
2023-11-08 09:05:55 +0700

    PR

--- day 10/pr.js
+++ day 10/pr.js
@@ -7,8 +7,118 @@
 // bisa checkout, pada saat checkout cart akan kosong dan stock akan berkurang
 // menu untuk menambahkan produk, edit produk, delete produk
 
-// produk, name, stock, category , warna
+// produk, name, stock, category , warna, price
 
 // category 2
 // pakaian => ukuran, brand
 // buku => author,totalPage
+
+// ada class tokopedia
+// tokopedia akan memiliki banyak produk yg beda category
+// produk yg ada bisa dipindahkan ke cart
+// setiap product punya stock masing masing
+// pada saat add to cart akan membutuhkan qty product (product,qty)
+// pada saat addtocart produk yg qty lebih kecil dari stock maka akan error
+// terdapat function checkout untuk membayar produk di keranjang
+// pada saat produk berhasil terbayar, cart akan kosong dan stock product akan berkurang
+
+class Product {
+  constructor(name, stock, category, warna, price) {
+    this.name = name;
+    this.stock = stock;
+    this.category = category;
+    this.warna = warna;
+    this.price = price;
+  }
+}
+
+class Pakaian extends Product {
+  constructor(name, stock, warna, price, size, brand) {
+    super(name, stock, "pakaian", warna, price); // constructor product
+    this.size = size;
+    this.brand = brand;
+  }
+}
+
+class Buku extends Product {
+  constructor(name, stock, warna, price, author, totalPage) {
+    super(name, stock, "buku", warna, price);
+    this.author = author;
+    this.totalPage = totalPage;
+  }
+}
+
+class Tokopedia {
+  #products = []; //menampung produk
+  #carts = []; // keranjang belanja
+
+  showProducts() {
+    //munculkan seluruh produk yg ada
+    this.#products.forEach((product) => console.log(product));
+  }
+  addProduct(product) {
+    if (product instanceof Product) {
+      const check = this.#products.findIndex((p) => p.name == product.name); //cek apakah produk yg mau diinput udah ada diproduk atau belum?
+      if (check == -1)
+        this.#products.push(product); //input produk ke dalam array products
+      else console.log("product sudah terdaftar");
+    } else {
+      console.log("product tidak jelas");
+    }
+  }
+  addToCart(nama, qty) {
+    const idxProduct = this.#products.findIndex(
+      (product) => product.name.toLowerCase() == nama.toLowerCase()
+    );
+
+    if (idxProduct == -1) return console.log("product tidak ditemukan");
+
+    const { name, price, stock } = this.#products[idxProduct];
+
+    if (qty > stock) return console.log("qty melebihi stock");
+    const idxCart = this.#carts.findIndex(
+      (product) => product.name.toLowerCase() == nama.toLowerCase()
+    );
+
+    if (idxCart == -1) this.#carts.push({ name, qty, price: qty * price });
+    else this.#carts.splice(idxCart, 1, { name, qty, price: qty * price });
+    console.log("product berhasil ditambahkan");
+  }
+  showCarts() {
+    if (!this.#carts.length) return console.log("cart kosong");
+    this.#carts.forEach((product) => console.log(product));
+  }
+  checkOut(payment = 0) {
+    if (!this.#carts.length) return console.log("cart masih kosong");
+    const total = this.#carts.reduce((sum, current) => sum + current.price, 0);
+    if (payment < total)
+      return console.log("uang anda kurang", total - payment);
+    else {
+      this.#carts.forEach((product) => {
+        const productIndex = this.#products.findIndex(
+          (val) => val.name == product.name
+        );
+        this.#products[productIndex].stock -= product.qty; // products[0].stock = 90
+      });
+      this.#carts = [];
+      console.log("kembalian anda", payment - total);
+    }
+  }
+}
+
+const tokped = new Tokopedia();
+tokped.addProduct(
+  new Buku("naruto", 100, "putih", 7000, "masashi kishimoto", 100)
+);
+tokped.addProduct(new Pakaian("kemeja H&M", 10, "biru", 300000, "xl", "H&M"));
+
+tokped.addProduct("hehehe");
+
+tokped.addToCart("naruto", 10);
+
+tokped.showProducts();
+// tokped.addToCart("naruto", 100);
+tokped.showCarts();
+tokped.checkOut(100000);
+tokped.showCarts();
+tokped.showProducts();
--- day 9/pr.js
+++ day 9/pr.js
@@ -15,31 +15,143 @@
 // 4. mengedit qty makanan tertentu di dalam cart
 
 //CRUD = > CREATE , READ (GET=>) , UPDATE ,DELETE
+class Food {
+  constructor(name, price, qty) {
+    this.name = name;
+    this.price = price;
+    this.qty = qty;
+  }
+}
+
+class GoFood {
+  #foods = [];
+  addFood(food) {
+    if (food instanceof Food) {
+      const check = this.#foods.findIndex((val) => val.name == food.name);
+      if (check == -1) this.#foods.push(food);
+      else {
+        this.#foods[check] = {
+          ...this.#foods[check],
+          qty: this.#foods[check].qty + food.qty,
+        };
+      }
+    } else {
+      console.log("bukan makanan");
+    }
+  }
+
+  showOrder() {
+    this.#foods.forEach((val) => console.log(val));
+  }
+
+  deleteOrder(foodName) {
+    const check = this.#foods.findIndex((val) => val.name == foodName);
+    if (check == -1) return console.log("makanan tidak ditemukan");
+    this.#foods.splice(check, 1);
+  }
+
+  editQty(foodName, qty) {
+    const check = this.#foods.findIndex((val) => val.name == foodName);
+    if (check == -1) return console.log("makanan tidak ditemukan");
+    this.#foods[check] = { ...this.#foods[check], qty };
+  }
+  total() {
+    console.log(
+      "total",
+      this.#foods.reduce((sum, curr) => sum + curr.price * curr.qty, 0)
+    );
+  }
+}
+
+const goFood = new GoFood();
+goFood.addFood(new Food("capcay", 50000, 2));
+goFood.addFood(new Food("bihun", 30000, 1));
+goFood.addFood(new Food("capcay", 50000, 2));
+goFood.deleteOrder("capcay");
+goFood.editQty("bihun", 50);
+goFood.showOrder();
+goFood.total();
 
 // nomor 2 => buat lah 5 function dengan parameter tinggi untuk menciptakan segitia bintang dengan menggunakan looping
 
 // expected output :
+
 // *
 // **
 // ***
 // ****
+let number = 5;
 
+for (let i = 0; i < number; i++) {
+  let star = ""; //i = 0, 1, 2
+  for (let j = 0; j <= i; j++) {
+    // 3 <=2 ? true
+    star += "*"; // *, **, ***
+  }
+  console.log(star);
+}
+console.log();
+
+// *****
 //  ****
 //   ***
 //    **
 //     *
 
+for (let i = 0; i < number; i++) {
+  star = ""; // i= 0 , 1, 2, 3,4, 5
+  for (let j = 0; j < number; j++) {
+    if (j < i) star += " "; // 5 < 4 ?
+    else star += "*"; // *****, " ****", "  ***", "   **", "    *"
+  }
+
+  console.log(star);
+}
+
 //     *
 //    **
 //   ***
 //  ****
+console.log();
 
-//  ****
-//  ***
-//  **
-//  *
+for (let i = 0; i < number; i++) {
+  star = ""; // i =0,1, 2,3, 4 ,5
+  for (let j = 0; j <= number; j++) {
+    if (j < number - i) star += " "; // 5 < 5 ? , 5 < 4, 5 < 3, 5 < 2 ,5< 1
+    else star += "*"; // "     *", "    **", "   ***", "  ****", " *****"
+  }
+  console.log(star);
+}
 
-//      *
-//     * *
-//    * * *
-//   * * * *
+console.log();
+// *****
+// ****
+// ***
+// **
+// *
+for (let i = 0; i < number; i++) {
+  star = ""; //i = 0 , 1 , 2, 3 , 4
+  for (let j = 0; j < number - i; j++) {
+    //5 < 5, 4 < 4, 3 < 3 ,0< 2 ,1< 1
+    star += "*"; // *****, ****, *** , **, *
+  }
+
+  console.log(star);
+}
+
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+for (let i = 1; i <= number; i++) {
+  star = ""; // i = 1, 2, 3,4, 5
+  //   console.log(i);
+  for (let j = 0; j < number; j++) {
+    if (number - i > j) star += " "; // 4 > 4, 3 > 4, 2>4, 1 > 4, 0> 4
+    else star += "* "; // "    * ", "   * * ", "  * * * ", " * * * * ", "* * * * * "
+  }
+
+  console.log(star);
+}
